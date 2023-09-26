ad03ed3f2017bfa39060c265960195fb8f7c391e
korndoe58 <korndoe58@gmail.com>
2023-09-26 11:47:10 +0700

    nav

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Image from "next/image";
+import CartIcon from "./CartIcon";
+
+const Navbar = () => {
+  const user = false;
+  return (
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
+      {/* LEFT LINKS */}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
+      {/* LOGO */}
+      <div className="text-xl md:font-bold flex-1 md:text-center">
+        <Link href="/">VAYUPAD CLAY</Link>
+      </div>
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+        <Menu />
+      </div>
+      {/* RIGHT LINKS */}
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className=" lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+          <Image src="/phone.svg" alt="" width={20} height={20} />
+          <span>123 456 78</span>
+        </div>
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+        <CartIcon/>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
